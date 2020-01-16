aff1a0b5fe1768d27278f93412c81564069f9ef1
Timothy H <timh1203@gmail.com>
2020-01-16 17:45:03 -0600

    Add Features component

---
+++ src/Features/Features.tsx
@@ -0,0 +1,12 @@
+// LOCAL IMPORTS
+import './Features.scss'
+
+const Features = () => {
+  return (
+    <main id="features">
+      <h1>Features</h1>
+    </main>
+  )
+}
+
+export default Features
