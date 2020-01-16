f2db13868ff3377122d6180242fb4a6348fa04c5
Timothy H <timh1203@gmail.com>
2020-01-15 19:19:01 -0600

    Add svg image

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -4,7 +4,18 @@ import './Header.scss'
 const Header = () => {
   return (
     <main id="header">
-      <img id="header-img" src="/static/pictekLogo.svg" alt="logo" />
+      <div className="header-col-1">
+        <img id="header-img" src="/static/pictekLogo.svg" alt="logo" />
+      </div>
+      <div className="header-col-2">
+        <nav id="nav-bar">
+          <ul>
+            <li>Features</li>
+            <li>Usage</li>
+            <li>Pricing</li>
+          </ul>
+        </nav>
+      </div>
     </main>
   )
 }
