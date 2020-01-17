e568bbc8027716669b73f212089056d262229604
Timothy H <timh1203@gmail.com>
2020-01-17 14:34:19 -0600

    Add Features content

--- src/Features/Features.tsx
+++ src/Features/Features.tsx
@@ -18,8 +18,65 @@ const Features = () => {
       </section>
       <section className="features-hero">
         <img id="header-img" src="/static/hero2.jpg" alt="Hero Banner" />
+        <ul>
+          <li>
+            【Multiple Lighting Modes Enhance the Game Atmosphere】
+            <br/>
+            PICTEK 87-Key compact gaming keyboard is designed with 27 types lighting modes to meet your needs of different game scenes, which also allows you to program your own backlight mode. The brightness, speed, direction are all available to change by specific keys.
+          </li>
+          <li>
+            【Ultimate Typing Feedback】
+            <br/>
+            This mechanical keyboard adopts professional equivalent switches, proving quicker and precise response, to win opportunities for you in the game. Blue switch offeres crisp sound, which is an enjoyment for gamers during the intense game. Comes with keycap puller for easy keycaps cleaning.
+          </li>
+          <li>
+            【Smooth Gaming Experience & Efficienct Typing】
+            <br/>
+            Full keys anti-ghosting design is more friendly to user, ensuring a sooth typing during gaming or working, reducing mistakes. 12 shortcuts makes you easily access to the email, calculator, player, my computer, as well as other media controlling functions, volume up/down, play/pause, stop, mute, previous/next track, upgarding the convenience and efficiency when using.
+          </li>
+          <li>
+            【Better Durability & Comfortable Design】
+            <br/>
+            Equipped with double-shot injection molded keycaps to ensure the letters on the kaycap will not fade. The wired gaming keyboard experienced 10, 000, 000 times keystrokes test to promise the durability. Foldable stand at the back of the keyboard allows you to choose the best typing height, reduce fatigue diring typing.
+          </li>
+          <li>
+            【Easy OF use & PICTEK service】
+            <br/>
+            this small gaming Keyboard is very easy and portable to use, Plug and play. This USB keyboard is perfectly compatible with Windows7/8/10/XP/Vista, Mac OS (Note: multimedia shortcuts are not available in Mac OS system). ❤❤PICTEK will spare no effort to provide the best service to every customer, feel free to Contact us anytime, We will reply you within 12 hours.
+          </li>
+        </ul>
       </section>
       <section className="features-blobs">
+        <div>
+          <img src="/static/blob1.jpg" alt="Keycap Puller" />
+          <h3>Included Keycap Puller </h3>
+          <p>PICTEK 87-key Mechanical gaming keyboard comes with a keycap puller to help clean the keyboard easier.</p>
+        </div>
+        <div>
+          <img src="/static/blob2.jpg" alt="Ergonomic Design" />
+          <h3>Ergonomic Design</h3>
+          <p>With keycaps leaning backward and forward design to make the typing more comfortable, the fold-out legs allows you to choose the best typing height.</p>
+        </div>
+        <div>
+          <img src="/static/blob3.jpg" alt="Anti-fading Keycaps" />
+          <h3>Fading Resistant Keycaps </h3>
+          <p>PICTEK keyboard adopts advanced craft to make sure the letter will not fade, enabling a long service life.</p>
+        </div>
+        <div>
+          <img src="/static/blob4.jpg" alt="Splash Resistsant" />
+          <h3>Splash Resistant</h3>
+          <p>Splash resistant helps to solve your worry about accident water or tea splash, three hydrophobic holes in the back of the keyboard to accelerate water stream.</p>
+        </div>
+        <div>
+          <img src="/static/blob5.jpg" alt="12 Multimedia Function Keys" />
+          <h3>12 Multimedia Function Keys</h3>
+          <p>12 Multimedia function keys to enhance efficiency during working on sheet, wps and playing.</p>
+        </div>
+        <div>
+          <img src="/static/blob6.jpg" alt="Lock the Win Key" />
+          <h3>Lock the Win Key</h3>
+          <p>Pressing the Win and Fn, you will be able to lock the page to help you focus on the game, avoiding page losing.</p>
+        </div>
       </section>
     </main>
   )
