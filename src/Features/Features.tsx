6b430ffd55caab4dc9bf305c701e6a28142a16cd
Timothy H <timh1203@gmail.com>
2020-01-16 22:52:40 -0600

    Add banner to Features

--- src/Features/Features.tsx
+++ src/Features/Features.tsx
@@ -7,11 +7,20 @@ import './Features.scss'
 const Features = () => {
   return (
     <main id="features">
+      <img className="features-banner" src="/static/hero1.jpg" alt="Feature Banner" />
       <h1>Features</h1>
-      <Youtube
-        videoId="nG4dL13PFrg"
-        opts={{ width: '640', height: '390' }}
-      />
+      <section className="features-video">
+        <Youtube
+          id="video"
+          videoId="nG4dL13PFrg"
+          opts={{ width: '640', height: '390' }}
+        />
+      </section>
+      <section className="features-hero">
+        <img id="header-img" src="/static/hero2.jpg" alt="Hero Banner" />
+      </section>
+      <section className="features-blobs">
+      </section>
     </main>
   )
 }
