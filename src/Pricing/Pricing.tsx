6bc24408b41d5ad59242ff30a4ede9e8ca8497f5
Timothy H <timh1203@gmail.com>
2020-01-18 15:38:04 -0600

    Add react hooks for form

--- src/Pricing/Pricing.tsx
+++ src/Pricing/Pricing.tsx
@@ -1,7 +1,32 @@
+// PACKAGE IMPORTS
+import axios from 'axios'
+
 // LOCAL IMPORTS
 import './Pricing.scss'
+import useFormFields from '../libs/hooksLib.js'
 
 const Pricing = () => {
+  const [info, setInfo] = useFormFields({
+    firstName: '',
+    lastName: '',
+    email: ''
+  })
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      await axios.post('https://www.freecodecamp.com/email-submit', info)
+        .then(res => console.log(res))
+    }
+    catch (err) {
+      console.error(err)
+    }
+    finally {
+      console.log("handleSubmit was processed.")
+    }
+  }
+
   return (
     <main id="pricing">
       <h1>Pricing</h1>
@@ -12,20 +37,41 @@ const Pricing = () => {
 
       <h3>Want to learn about it or have questions?</h3>
       <p>Feel free to email me!</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="for-group group1">
           <label htmlFor="firstName">First Name:</label>
-          <input id="firstName" name="firstName" type="text" placeholder="Type your first name" required />
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            placeholder="Type your first name"
+            value={info.firstName}
+            onChange={setInfo}
+            autoFocus
+            required
+          />
         </div>
         <div className="for-group group2">
           <label htmlFor="lastName">Last Name:</label>
-          <input id="lastName" name="lastName" type="text" placeholder="Type your last name" required />
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            placeholder="Type your last name"
+            onChange={setInfo}
+            required />
         </div>
         <div className="for-group group3">
           <label htmlFor="email">Email:</label>
-          <input id="email" name="email" type="email" placeholder="Type your email" required />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Type your email"
+            onChange={setInfo}
+            required />
         </div>
-        <input id="submit" type="button" value="Submit" />
+        <input id="submit" type="submit" value="Submit" />
       </form>
     </main>
   )
