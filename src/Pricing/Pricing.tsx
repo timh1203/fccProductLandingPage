05325abf39cb4117d8cd38c28275bef6cce0cfd3
Timothy H <timh1203@gmail.com>
2020-01-21 12:27:23 -0600

    Adjust pricing section

--- src/Pricing/Pricing.tsx
+++ src/Pricing/Pricing.tsx
@@ -31,47 +31,56 @@ const Pricing = () => {
     <main id="pricing">
       <h1>Pricing</h1>
 
-      <h3>An Amazon Choice</h3>
-      <p>Price: $28.99 with Free Shipping Included</p>
-      <p>$2 dollar coupon available</p>
+      <div className="pricing-col">
+        <div className="pricing-col-1">
+          <img src="../static/amazonChoice.png" alt="Amazon Choice" />
+        </div>
+        <div className="pricing-col-2">
+          <p><span>Price</span>: $28.99 (Includes Free Shipping)</p>
+          <p><span>Special Promotion</span>: $2 dollar coupon available</p>
+        </div>
+      </div>
 
-      <h3>Want to learn about it or have questions?</h3>
-      <p>Feel free to email me!</p>
-      <form onSubmit={handleSubmit}>
-        <div className="for-group group1">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            placeholder="Type your first name"
-            value={info.firstName}
-            onChange={setInfo}
-            required
-          />
-        </div>
-        <div className="for-group group2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            placeholder="Type your last name"
-            onChange={setInfo}
-            required />
-        </div>
-        <div className="for-group group3">
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="Type your email"
-            onChange={setInfo}
-            required />
-        </div>
-        <input id="submit" type="submit" value="Submit" />
-      </form>
+      <div className="pricing-contact">
+        <h3>Have questions before you hit <button>buy</button>?</h3>
+        &nbsp;
+        <p>Feel free to email me!</p>
+        <form onSubmit={handleSubmit}>
+          <div className="for-group group1">
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              id="firstName"
+              name="firstName"
+              type="text"
+              placeholder="Type your first name"
+              value={info.firstName}
+              onChange={setInfo}
+              required
+            />
+          </div>
+          <div className="for-group group2">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              id="lastName"
+              name="lastName"
+              type="text"
+              placeholder="Type your last name"
+              onChange={setInfo}
+              required />
+          </div>
+          <div className="for-group group3">
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Type your email"
+              onChange={setInfo}
+              required />
+          </div>
+          <input id="submit" type="submit" value="Submit" />
+        </form>
+      </div>
     </main>
   )
 }
